4bc785582039426f166cadbd033c6fc0e2c55ff5
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2016-11-21 12:22:49 -0300

    Agrega setup de la discretizacion

--- relajacion.py
+++ relajacion.py
@@ -3,3 +3,12 @@
 """
 Descripcion del problema.
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Setup
+LX = 2
+LY = 2
+Np = 5 # numero de puntos para discretizacion
+h = LX / (Np - 1)
